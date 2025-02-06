6bff1d2ea4269d075cd61c7a61d1d718ff15b897
vaishali05d <sharma.vaishali.0512@gmail.com>
2025-02-06 16:54:34 +0530

    cors problem solved

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,26 @@
 const express = require('express');
-const cors = require('cors'); 
+const cors = require('cors');
 require('dotenv').config();
 const dbConfig = require('./config/dbConfig');
 
 const app = express();
-const allowedOrigins = ["https://my-mern-portfolioo-ui.vercel.app"];
 
 app.use(cors({
-  origin: allowedOrigins, 
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true, 
+  origin: "https://my-mern-portfolio-ui.vercel.app", 
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
 }));
-
 
 app.use(express.json());
 
 const portfolioRoute = require("./routes/portfolioRoute");
 app.use("/api/portfolio", portfolioRoute);
 
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
